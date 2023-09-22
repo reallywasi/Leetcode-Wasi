27c8ebed7fd44c8d196296654806b7a951128f3a
Mohammad Wasi <reallywasi@gmail.com>
2023-09-22 18:07:02 +0530

    Update SearchInsertPosition.cpp

--- SearchInsertPosition.cpp
+++ SearchInsertPosition.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+     {
+int start=0;
+int end = nums.size();
+int mid;
 
+if(target>nums[end-1])
+{
+    return end;
+
+}
+ while(start<=end)
+
+   {
+mid=start+(end-start)/2;
+
+if(target==nums[mid])
+{
+return mid;
+}
+
+ if (target<nums[mid])
+{
+end=mid-1;
+
+}
+else
+{
+start=mid+1;
+}
+
+}
+return start;
+
+     }
+
+    
+};
